bff0a81481a7079354631d9e9d1016c1bd346a01
NE6091043 <72957935+NE6091043@users.noreply.github.com>
2021-09-15 11:06:46 +0800

    Create q_learning_v.py

---
+++ python/q_learning_v.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 25 17:52:29 2021
+
+@author: ne6091043
+"""
+
+from flask import Flask, request
+# 用不到gym環境，環境為OM2M
+# from agent import Env, TestEnv
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import gym
+from tqdm import tqdm
+import time
+from gym import spaces
+from gym.utils import seeding
+import random
+import pandas as pd
+
+app = Flask(__name__)
+
+# Initialize the environment of our problem
+# num_protocols = 4
+#protocols = ['COAP', 'MQTT', 'WebSocket', 'XMPP']
+
+# average delay(ms) of data bytes between 100、500、1000、1500 event-driven in packet loss rate 0 %  means
+# coap:26.705  mqtt:82.5635  ws:105.82  xmpp:96.9525
+
+# get data from gscl and return action based on test_update
+
+
+class QLearningTable:
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        self.actions = actions  # a list
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+        
+
+    def choose_action(self, observation):
+        self.check_state_exist(observation)
+        # action selection
+        if np.random.uniform() < self.epsilon:
+            # choose best action
+            #print(['COAP', 'MQTT', 'WebSocket', 'XMPP'])
+            print(self.q_table)
+            state_action = self.q_table.loc[observation, :]
+            print(state_action)
+            # some actions may have the same value, randomly choose on in these actions
+            # print(state_action[state_action == np.max(state_action)])
+            # print(state_action[state_action == np.max(state_action)].index)
+            action = np.random.choice(state_action[state_action == np.max(state_action)].index)
+        else:
+            # choose random action
+            action = np.random.choice(self.actions)
+        print(action)
+        print('8888888888888888888888888888888888')
+        return action
+
+    def learn(self, s, a, r, s_):
+        self.check_state_exist(s_)
+        q_predict = self.q_table.loc[s, a]
+        q_target = r + self.gamma * self.q_table.loc[s_, :].max()  # next state is not terminal
+        self.q_table.loc[s, a] += self.lr * (q_target - q_predict)  # update
+
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            # append new state to q table
+            self.q_table = self.q_table.append(
+                pd.Series(
+                    [0]*len(self.actions),
+                    index=self.q_table.columns,
+                    name=state,
+                )
+            )
+
+
+agent=QLearningTable(actions=list(range(4)))
+
+
+
+def change_state_range(size):
+    if 1500<size<1999:
+        return 1
+    elif 1999<size<2500:
+        return 2
+    elif 2501<size<2999:
+        return 3
+    else:
+        return 4
+        
+
+# define init state
+# state = [data size , avg loss rate before this time]
+# idx for the state order
+idx=0
+action = np.int64(0)
+s=[]
+immreward=0
+
+@app.route('/get_data_size_return_action', methods=['POST'])
+def get_data_size_return_action():
+    
+    # data[0]=datasize,data[1]=idx  for next state
+    data = request.data.decode().split('//')
+    global idx,action,s,immreward
+    # learn agent from state before
+    if idx!=0 and idx+1==float(data[1]):
+        s_=[change_state_range(float(data[0])),0.0]
+        agent.learn(str(s),action,immreward,str(s_))
+    
+    size=float(data[0])
+    idx=int(data[1])
+    
+    # state = [data size , avg loss rate before this time]
+    s=[change_state_range(size),0.0]
+    
+    action = agent.choose_action(str(s))
+    # print('..', size)
+    # print("ok")
+    # print('--------------***********************------------------')
+    # print(action)
+    # print('--------------***********************------------------')
+    if action.item() == 0:
+        #print("coap")
+        return "coap"
+    if action.item() == 1:
+        #print("mqtt")
+        return "mqtt"
+    if action.item() == 2:
+        #print("ws")
+        return "ws"
+    if action.item() == 3:
+        return "xmpp"
+
+# update reward 
+# not returning anything
+
+
+@app.route('/receive_delay_as_reward', methods=['POST'])
+def receive_action_and_delay_as_reward():
+    global idx,immreward
+    # state s_ is new observation
+    # set immediate reward as delay/data_size
+    data = request.data.decode().split('//')
+    delay=-float(data[0])
+    if idx==int(data[1]):
+        immreward=delay
+    # res = data.split('//')
+    # res[0]=delay
+    # res[1]=protocol
+
+    # print(res[0])
+    # print(res[1])
+    # imm_reward = res[0]/res[1]
+    return "aaaa"
+
+
+if __name__ == '__main__':
+    # app.debug = True
+    app.run(host='140.116.247.69', port=9000)
